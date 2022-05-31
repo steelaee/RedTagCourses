f213b3a6bc563f676dc4f29298714ade00469938
steelaee <isepianov@gmail.com>
2022-05-31 12:47:58 +0100

    Task8 Update

--- force-app/main/default/aura/AuraContractPDF/AuraContractPDFController.js
+++ force-app/main/default/aura/AuraContractPDF/AuraContractPDFController.js
@@ -1,12 +1,5 @@
 ({
     doInit : function(component, event, helper) {
-        var action = component.get('c.generatePDF');
-
-        action.setCallback(this, function(response){
-            var state = response.getState();
-            if(state == 'SUCCESS'){
-                component.set('v.accList', response.getReturnValue());
-            }
-        })
+        helper.generatePDFHelper(component);
     }
 })
--- force-app/main/default/aura/AuraContractPDF/AuraContractPDFHelper.js
+++ force-app/main/default/aura/AuraContractPDF/AuraContractPDFHelper.js
@@ -1,5 +1,16 @@
 ({
-    helperMethod : function() {
+    generatePDFHelper : function(component) {
+        var action = component.get('c.generatePDF');
+        action.setParams({
+            "recordId": component.get("v.recordId")
+        });
+        action.setCallback(this, function(response){
+            var state = response.getState();
+            if(state == 'SUCCESS'){
+                component.set('v.accList', response.getReturnValue());
+            }
+        });
 
+        $A.enqueueAction(action);
     }
 })
