3889be58ab97d37658cda84bf02dc7e07c777428
steelaee <isepianov@gmail.com>
2022-06-30 09:46:02 +0300

    Task9 Commit

--- force-app/main/default/lwc/weatherDisplay/weatherDisplay.js
+++ force-app/main/default/lwc/weatherDisplay/weatherDisplay.js
@@ -1,36 +1,54 @@
 import { LightningElement, track } from 'lwc';
-import getCityWeatherRecord from '@salesforce/apex/WeatherController.getCityWeatherRecord';
-import getLocations from '@salesforce/apex/WeatherAPIService.getLocations';
+import getCityWeatherRecords from '@salesforce/apex/WeatherController.getCityWeatherRecords';
+import getSettings from '@salesforce/apex/WeatherAPIService.getSettings';
 
 export default class WeatherComponent extends LightningElement {
     @track mapMarkers = [];
-    weather;
-    options = [];
+    @track columns = [
+        { label: 'City Name', fieldName: 'cityName'},
+        { label: 'Date', fieldName: 'date'},
+        { label: 'Temperature', fieldName: 'temperature'},
+        { label: 'Pressure', fieldName: 'pressure'},
+        { label: 'Temperature feels like', fieldName: 'temperatureFeelsLike'},
+        { label: 'Wind Speed', fieldName: 'windSpeed'}
+    ];
+    @track weather;
+    @track data = [];
+    currentCity;
     
     connectedCallback(){
-        getLocations().then(response => {
-            for(let i = 0; i < response.length; i++){
+        getSettings().then(response => {
+            response.map((el) => {
                 let marker = {
                     location: {
-                        City: response[i]
-                    }
+                        City: el.City_Name__c,
+                        Latitude: el.Coordinates__latitude__s,
+                        Longitude: el.Coordinates__longitude__s
+                    },
+                    value: el.City_Name__c
                 };
                 this.mapMarkers = [...this.mapMarkers, marker];
-
-
-                
-                let option = {
-                    label: response[i],
-                    value: response[i]
-                };
-                this.options = [...this.options, option];
-            }   
+            });
         });
     }
-    handleChange(event){
-        let value = event.detail.value;
-        getCityWeatherRecord({city: value}).then(response => {
-            this.weather = response;
+
+    markerSelect(event){
+        this.currentCity = event.target.selectedMarkerValue;
+        this.data = [];
+        getCityWeatherRecords({city:this.currentCity}).then(response => {
+            this.weather = response[0];
+
+            response.map((weath) => {
+                let el ={
+                    cityName: weath.City_Name__c,
+                    date: weath.Date__c,
+                    temperature: weath.Temperature__c,
+                    pressure: weath.Pressure__c,
+                    temperatureFeelsLike: weath.Temp_Feels_Like__c,
+                    windSpeed: weath.Wind_Speed__c
+                };
+                this.data = [...this.data, el];
+            });
         })
     }
 }
