6a19fe631fb267b6cb7615d7cea8721525c75a24
steelaee <isepianov@gmail.com>
2022-07-18 18:38:12 +0300

    Task11 Commit

--- force-app/main/default/lwc/caseSplit/caseSplit.js
+++ force-app/main/default/lwc/caseSplit/caseSplit.js
@@ -1,30 +1,35 @@
 import { LightningElement, track, wire } from 'lwc';
-import getCases from '@salesforce/apex/CaseController.getCases';
-import changeCaseStatus from '@salesforce/apex/CaseController.changeCaseStatus';
-import { subscribe, unsubscribe, onError } from 'lightning/empApi';
-import VINE_BOOM from '@salesforce/resourceUrl/VineBoom';
-import USERID from '@salesforce/user/Id';
+import getCases from '@salesforce/apex/CaseService.getCases';
+import changeCaseStatus from '@salesforce/apex/CaseService.changeCaseStatus';
 import { getRecord } from 'lightning/uiRecordApi';
-import BRANCHNUM from '@salesforce/schema/User.Branch_Number__c';
+import { subscribe, onError } from 'lightning/empApi';
+import VINE_BOOM from '@salesforce/resourceUrl/VIneBoom';
+import USER_ID from '@salesforce/user/Id';
+import USER_BRANCHNUMBER from '@salesforce/schema/User.Branch_Number__c';
+import CASE_BRANCHNUMBER from '@salesforce/schema/Case.Branch_Number__c';
+import CASE_CASENUMBER from '@salesforce/schema/Case.CaseNumber';
+import CASE_CREATEDDATE from '@salesforce/schema/Case.CreatedDate';
+import CASE_SUBJECT from '@salesforce/schema/Case.Subject';
+import CASE_TYPE from '@salesforce/schema/Case.Type';
+import CASE_STATUS from '@salesforce/schema/Case.Status';
 
+const channelName = '/event/Case_Create__e';
 export default class CaseSplit extends LightningElement {
-    channelName = '/event/Case_Create__e';
-    subscription = {};
     userBranchNum;
+    selectedCaseId;
     columns = [
-        {label: 'Case Number', fieldName: 'num', type: 'button', typeAttributes: {label: {fieldName: 'num'}, variant: 'base'}},
-        {label: 'Branch Number', fieldName: 'branchNumber'},
-        {label: 'Created Date', fieldName: 'createdDate'},
-        {label: 'Subject', fieldName: 'subject'},
-        {label: 'Type', fieldName: 'type'},
-        {label: 'Status', fieldName: 'status'}
+        {label: 'Case Number', fieldName: CASE_CASENUMBER, type: 'button', typeAttributes: {label: {fieldName: CASE_CASENUMBER}, variant: 'base'}},
+        {label: 'Branch Number', fieldName: CASE_BRANCHNUMBER},
+        {label: 'Created Date', fieldName: CASE_CREATEDDATE},
+        {label: 'Subject', fieldName: CASE_SUBJECT},
+        {label: 'Type', fieldName: CASE_TYPE},
+        {label: 'Status', fieldName: CASE_STATUS}
     ];
     @track caseOpened = false;
     @track cases = [];
-    @track case = {};
     @track isLoading = false;
 
-    @wire(getRecord, {recordId: USERID, fields: [BRANCHNUM]})
+    @wire(getRecord, {recordId: USER_ID, fields: [USER_BRANCHNUMBER]})
     setUserBranchNum({data, error}){
         if(data){
             this.userBranchNum = data.fields.Branch_Number__c.value;
@@ -34,28 +39,17 @@ export default class CaseSplit extends LightningElement {
         }
     }
 
-    get caseId(){
-        return this.case.id;
-    };
-
     wireCases(){
-        getCases().then(response => {
-            this.cases = response.map((cs) => {
-                let el = {
-                    id: cs.Id,
-                    num: cs.CaseNumber,
-                    branchNumber: cs.Branch_Number__c,
-                    createdDate: cs.CreatedDate,
-                    status: cs.Status,
-                    type: cs.Type,
-                    subject: cs.Subject
-                };
-                return el;
-            });
-            this.cases.sort((a,b) => (a.createdDate < b.createdDate) ? 1 : ((b.createdDate < a.createdDate) ? -1 : 0));
+        this.isLoading = true;
+        getCases({
+            branchNum: this.userBranchNum
+        }).then(response => {
+            this.cases = response;
         }).catch(error => {
-            console.error('wireCases: ', error);
-        })
+            console.error('wireCases:', error);
+        }).finally(() => {
+            this.isLoading = false;
+        });
     }
 
     connectedCallback(){
@@ -77,10 +71,7 @@ export default class CaseSplit extends LightningElement {
             }
         };  
 
-        subscribe(this.channelName, -1, messageCallback)
-        .then(response => {
-            this.subscription = response;
-        })
+        subscribe(channelName, -1, messageCallback)
         .catch(error => {
             console.error('handleSubscribe:', error);
         }); 
@@ -96,20 +87,19 @@ export default class CaseSplit extends LightningElement {
         this.isLoading = true;
         this.template.querySelector('[data-id="table-div"]').classList.add('slds-size_2-of-3');
 
-        this.case = this.cases.find(obj => {
-            return obj.id === event.detail.row.id;
-        });
+        this.selectedCaseId = event.detail.row.id;
 
-        changeCaseStatus({caseId: this.caseId})
-        .then(() => {
+        changeCaseStatus({
+            caseId: this.caseId
+        }).then(() => {
             this.wireCases();
-        })
-        .catch(error => {
+        }).catch(error => {
             console.error('handleNumClick:', error);
+        }).finally(() => {
+            this.caseOpened = true;
+            this.isLoading = false;
         });
         
-        this.caseOpened = true;
-        this.isLoading = false;
     }
 
     handleCloseClick(){
